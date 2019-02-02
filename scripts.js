ffde58e800bc5f591366394710ac94e77041e76b
sylwiavv <sylwiavv@gmail.com>
2019-02-02 17:36:19 +0100

    Edit footer

---
+++ scripts.js
@@ -0,0 +1,15 @@
+<script>
+  // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 140 || document.documentElement.scrollTop > 140) {
+    document.getElementById("back2Top").style.display = "block";
+  } else {
+    document.getElementById("back2Top").style.display = "none";
+  }
+
+
+
+
+</script>
